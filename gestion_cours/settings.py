b8530bd0ef582b0632af4ebbd4f44c82f66933ac
Steph <stephanedargani@gmail.com>
2026-03-22 21:03:51 +0000

    Variables environnement production

--- gestion_cours/settings.py
+++ gestion_cours/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&s7qyuc^^5v-k6+0gh_w@*i%z3-e01syf$pppd+099teromk(*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-local')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 
