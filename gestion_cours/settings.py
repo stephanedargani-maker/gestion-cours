db2ca5a2d66412bc458463c0061a95b1a1f12007
Steph <stephanedargani@gmail.com>
2026-03-22 21:29:04 +0000

    Fix staticfiles for Railway

--- gestion_cours/settings.py
+++ gestion_cours/settings.py
@@ -130,5 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
