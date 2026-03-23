435bb3de106630c9dc740f49d681614564a1fd6d
Steph <stephanedargani@gmail.com>
2026-03-23 00:11:10 +0000

    Fix static files production

--- gestion_cours/settings.py
+++ gestion_cours/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
